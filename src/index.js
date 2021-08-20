078a5f681cffd9b24d5f9e7ae69467302b8cdcf0
PavelAkulovich <pasha860@mail.ru>
2021-08-20 14:25:28 +0300

    feat:task is finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+      return 0;
+  }
+  let valMin = Infinity;
+  for ( i = 0; i < array.length; i++) {
+      if ( array[i] < valMin ) {
+      valMin = array[i];
+      }
+      
+  }
+ return valMin || 0;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }
+
+  let valMax = -Infinity;
+  for ( i = 0; i < array.length; i++) {
+      if ( array[i] > valMax ) {
+      valMax = array[i];
+      }
+      
+  }
+ return valMax;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }
+    let valAvg = 0;
+    for ( i = 0; i < array.length; i++) {
+        valAvg = array[i] + valAvg;
+        }
+   return valAvg/array.length;
+  
 }
